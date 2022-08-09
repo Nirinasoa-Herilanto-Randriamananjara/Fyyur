4128a3885a9695583086d4034437fdeadeb410a0
Nhr98 <nirinasoa.herilanto@gmail.com>
2022-08-09 21:07:31 +0300

    init module

---
+++ init.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_moment import Moment
+
+
+app = Flask(__name__)
+
+moment = Moment(app)
+
+app.config.from_object('config')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)

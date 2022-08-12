84bef76d5b2169fcbd5fa38431966864faaf8125
Nhr98 <nirinasoa.herilanto@gmail.com>
2022-08-12 21:20:57 +0300

    UPDATE: add CSRFProtect

--- init.py
+++ init.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
-
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 moment = Moment(app)
 
